96dbb041557f4bcfb41bc92fa937f7f4cd537ffb
Hawken Rives <hawkrives@gmail.com>
2015-04-13 00:40:57 -0500

    upgrade join

--- join.hpp
+++ join.hpp
@@ -7,12 +7,32 @@ using std::to_string;
 #include <vector>
 using std::vector;
 
+string join(string sep, vector<string> coll) {
+	string str;
+	string comma = "\0";
+	for (const auto item : coll) {
+		str += comma;
+		str += item;
+		comma = sep;
+	}
+	return str;
+}
+
 template<typename T>
 string join(string sep, vector<T> coll) {
 	string str;
-	for (auto item : coll)
-		str += to_string(item) + sep;
+	string comma = "\0";
+	for (auto item : coll) {
+		str += comma;
+		str += to_string(item);
+		comma = sep;
+	}
 	return str;
+}
+
+template<typename T>
+string join(int sep, vector<T> coll) {
+	return join(to_string(sep), coll);
 }
 
 #endif
